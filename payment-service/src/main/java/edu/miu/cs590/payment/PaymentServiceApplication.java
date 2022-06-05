d5c8d77d00f10ead26539bea4f25ad6d3d492ac7
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-06-04 21:46:18 -0500

    change datasource from mysql to postgreSQL

--- payment-service/src/main/java/edu/miu/cs590/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/edu/miu/cs590/payment/PaymentServiceApplication.java
@@ -2,19 +2,16 @@ package edu.miu.cs590.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 
 }
--- payment-service/src/main/java/edu/miu/cs590/payment/service/PaymentServiceImpl.java
+++ payment-service/src/main/java/edu/miu/cs590/payment/service/PaymentServiceImpl.java
@@ -11,8 +11,6 @@ import java.util.UUID;
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
-    @Autowired
-    RestTemplate restTemplate;
 
     @Autowired
     PaymentRepository paymentRepository;
