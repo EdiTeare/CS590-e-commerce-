f407d3c4cc9b6a5b1d4f81343938880118381053
Nebyat Tibebu <nebyatweb@users.noreply.github.com>
2022-06-05 15:55:41 -0500

    update port number -> Order-Service and add histrix annotations - Gateway

--- cloud-gateway/src/main/java/edu/miu/cs590/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/edu/miu/cs590/cloud/gateway/CloudGatewayApplication.java
@@ -3,17 +3,11 @@ package edu.miu.cs590.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
-=======
-
-@SpringBootApplication
-@EnableEurekaClient
->>>>>>> b9ca64b5f888dbac668e273736dd13055bec45f0
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
